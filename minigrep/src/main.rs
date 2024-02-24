cb885b095460c0fc4922af9a05cd211a9b316cf2
Dipshan Adhikari <dipshanadh@gmail.com>
2024-02-24 16:40:48 +0545

    Split into library crate

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,7 @@
-use std::{env, error::Error, fs, process};
+use std::env;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,37 +11,11 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for: {}\n", config.query);
-    println!("In file: {}\n", config.file_path);
+    println!("Searching for: {}", config.query);
+    println!("In file: {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
     }
 }
